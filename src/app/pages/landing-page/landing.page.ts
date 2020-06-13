863b4aa16f1c66f015d953a9c7860e22e142c0de
Emma Livingstone <emmalivingstone94@gmail.com>
2020-06-13 15:01:53 +0100

    Adding ignored local env file for API key Moving app markup to landing page component

---
+++ src/app/pages/landing-page/landing.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dsgn-landing',
+  templateUrl: './landing.page.html',
+  styleUrls: ['./landing.page.scss'],
+})
+export class LandingPageComponent {}
