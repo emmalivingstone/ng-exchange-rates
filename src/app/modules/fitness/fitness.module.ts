8e621bbfc75776a46842e33adba63600a26519d3
Emma Livingstone <emmalivingstone94@gmail.com>
2020-06-19 17:20:57 +0100

    Adding dashboard and card components

--- src/app/modules/fitness/fitness.module.ts
+++ src/app/modules/fitness/fitness.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { FitnessRoutingModule } from './fitness.routing';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, FitnessRoutingModule],
+  declarations: [DashboardComponent],
+  imports: [CommonModule, FitnessRoutingModule, SharedModule],
   exports: [],
   providers: [],
 })
