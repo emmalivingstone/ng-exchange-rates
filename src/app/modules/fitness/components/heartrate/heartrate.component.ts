746c0e5adddcb5ab61f9dc6f3ab3db4dfdae415c
Emma Livingstone <emmalivingstone94@gmail.com>
2020-07-02 16:04:19 +0100

    Adding new size vars Adding size prop to card Adding heartrate component

---
+++ src/app/modules/fitness/components/heartrate/heartrate.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dsgn-heartrate',
+  templateUrl: './heartrate.component.html',
+  styleUrls: ['./heartrate.component.scss'],
+})
+export class HeartrateComponent {}
--- src/app/modules/fitness/fitness.module.ts
+++ src/app/modules/fitness/fitness.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HeartrateComponent } from './components/heartrate/heartrate.component';
 import { FitnessRoutingModule } from './fitness.routing';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, HeartrateComponent],
   imports: [CommonModule, FitnessRoutingModule, SharedModule],
   exports: [],
   providers: [],
