389f082aee9eb009e89c6416923a17d07fd0936c
Emma Livingstone <emmalivingstone94@gmail.com>
2020-07-14 10:04:38 +0100

    Adding water component Setting up dashboard CSS grid

--- src/app/modules/fitness/fitness.module.ts
+++ src/app/modules/fitness/fitness.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HeartrateComponent } from './components/heartrate/heartrate.component';
+import { WaterComponent } from './components/water/water.component';
 import { FitnessRoutingModule } from './fitness.routing';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [DashboardComponent, HeartrateComponent],
+  declarations: [DashboardComponent, HeartrateComponent, WaterComponent],
   imports: [CommonModule, FitnessRoutingModule, SharedModule],
   exports: [],
   providers: [],
